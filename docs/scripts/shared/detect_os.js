8fca13c708fd42dec2581a904c0cd435705edef7
ThanasisGeorg <thanasisgeorg03@gmail.com>
2024-02-12 13:19:44 +0200

    change font for win

--- docs/scripts/shared/detect_os.js
+++ docs/scripts/shared/detect_os.js
@@ -1,7 +1,7 @@
 let userAgent = window.navigator.userAgent;
 
 if (userAgent.indexOf("Win") !== -1) {
-	document.body.style.fontFamily = "Liberation Sans"
+	document.body.style.fontFamily = "Segoe UI"
 } else if (userAgent.indexOf("Linux") != -1) {
 	document.body.style.fontFamily = "Manjari"
 }
